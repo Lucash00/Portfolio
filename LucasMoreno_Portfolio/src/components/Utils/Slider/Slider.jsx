762a8e055e7e44d079a6ca4a58742d8bc33fdfe2
Lucash00 <lucas.moreno.dev@gmail.com>
2024-06-24 00:24:33 +0200

    retoque de slider y card

--- LucasMoreno_Portfolio/src/components/Utils/Slider/Slider.jsx
+++ LucasMoreno_Portfolio/src/components/Utils/Slider/Slider.jsx
@@ -1,8 +1,8 @@
-// Slider.jsx
 import React, { useState } from "react";
 
 const Slider = ({ images }) => {
   const [currentIndex, setCurrentIndex] = useState(0);
+  const [hoveredDot, setHoveredDot] = useState(null);
 
   const nextImage = () => {
     setCurrentIndex((prevIndex) => (prevIndex === images.length - 1 ? 0 : prevIndex + 1));
@@ -12,33 +12,90 @@ const Slider = ({ images }) => {
     setCurrentIndex((prevIndex) => (prevIndex === 0 ? images.length - 1 : prevIndex - 1));
   };
 
+  const handleClickDot = (index) => {
+    setCurrentIndex(index);
+  };
+
+  const handleArrowClick = (direction) => {
+    if (direction === 'prev') {
+      prevImage();
+    } else if (direction === 'next') {
+      nextImage();
+    }
+  };
+
   return (
     <div className="relative w-full">
-      {/* Imagen actual */}
-      <img
-        src={images[currentIndex]}
-        alt={`Image ${currentIndex}`}
-        className="object-contain mx-auto"
-      />
+      {/* Contenedor de las imágenes */}
+      <div className="relative w-full bg-gray-50">
+        {/* Imagen actual */}
+        <img
+          src={images[currentIndex]}
+          alt={`Image ${currentIndex}`}
+          className="object-contain mx-auto"
+        />
 
-      {/* Imagen anterior */}
-      <img
-        src={images[currentIndex === 0 ? images.length - 1 : currentIndex - 1]}
-        alt={`Previous Image`}
-        className="absolute left-0 top-0 opacity-50 mx-[10%]"
-      />
+        {/* Imagen anterior */}
+        <img
+          src={images[currentIndex === 0 ? images.length - 1 : currentIndex - 1]}
+          alt={`Previous Image`}
+          className="absolute left-0 top-0 opacity-40 mx-[10%]"
+        />
 
-      {/* Imagen siguiente */}
-      <img
-        src={images[currentIndex === images.length - 1 ? 0 : currentIndex + 1]}
-        alt={`Next Image`}
-        className="absolute right-0 top-0 opacity-50 mx-[10%]"
-      />
+        {/* Imagen siguiente */}
+        <img
+          src={images[currentIndex === images.length - 1 ? 0 : currentIndex + 1]}
+          alt={`Next Image`}
+          className="absolute right-0 top-0 opacity-40 mx-[10%]"
+        />
 
-      {/* Botones para cambiar de imagen */}
-      <div className="absolute top-0 left-0 w-full h-full flex items-center justify-between">
-        <button className="w-[5%] h-full bg-black bg-opacity-25" onClick={prevImage}></button>
-        <button className="w-[5%] h-full bg-black bg-opacity-25" onClick={nextImage}></button>
+        {/* Botones para cambiar de imagen */}
+        <div className="absolute top-0 left-0 w-full h-full flex items-center justify-between">
+          <button
+            className="w-[5%] h-full bg-gray-300 focus:outline-none flex items-center justify-center hover:bg-gray-400 transition duration-300 transform relative"
+            onClick={() => handleArrowClick('prev')}
+          >
+            <span className="text-white text-2xl">{'<'}</span>
+            <span className="absolute top-0 left-0 w-full h-full rounded-full border-2 border-transparent transition-all duration-300 transform scale-0 "></span>
+          </button>
+          <button
+            className="w-[5%] h-full bg-gray-300 focus:outline-none flex items-center justify-center hover:bg-gray-400 transition duration-300 transform relative"
+            onClick={() => handleArrowClick('next')}
+          >
+            <span className="text-white text-2xl">{'>'}</span>
+            <span className="absolute top-0 left-0 w-full h-full rounded-full border-2 border-transparent transition-all duration-300 transform scale-0 "></span>
+          </button>
+        </div>
+      </div>
+
+      {/* Marcadores (dots) fuera del contenedor principal */}
+      <div className="flex justify-center mt-4">
+        {images.map((image, index) => (
+          <div key={index} className="relative">
+            <button
+              onClick={() => handleClickDot(index)}
+              onMouseEnter={() => setHoveredDot(index)}
+              onMouseLeave={() => setHoveredDot(null)}
+              className={`rounded-full mx-1 focus:outline-none transition-all duration-300 ${
+                index === currentIndex ? 'bg-gray-500' : 'bg-gray-300'
+              }`}
+              style={{
+                width: index === currentIndex ? '12px' : '10px',
+                height: index === currentIndex ? '12px' : '10px',
+                transform: hoveredDot === index ? 'scale(1.5)' : 'scale(1)',
+              }}
+            />
+            {hoveredDot === index && (
+              <div className="absolute bottom-full mb-2 w-24 h-24 transform -translate-x-1/2 left-1/2">
+                <img
+                  src={image}
+                  alt={`Thumbnail ${index}`}
+                  className="object-contain w-full h-full rounded-lg"
+                />
+              </div>
+            )}
+          </div>
+        ))}
       </div>
     </div>
   );
