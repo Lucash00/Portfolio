118420ca06f5a9497796a14a8ac08240d0b9c420
Lucash00 <lucas.moreno.dev@gmail.com>
2025-04-18 12:30:39 +0200

    Add animations to layout components and configure Tailwind CSS for fadeIn and slideIn effects

--- LucasMoreno_Portfolio/tailwind.config.mjs
+++ LucasMoreno_Portfolio/tailwind.config.mjs
@@ -3,62 +3,75 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-	  extend: {
-		zIndex: {
-			'0': 0,
-			'10': 10,
-			'50': 50,
-			'1000': 1000,
-			'9999': 9999,
-		  },
-		spacing: {
-		  '100': '25rem',
-		},
-		fontFamily: {
-		  Poppins: ["Poppins", "sans-serif"],
-		},
-		fontSize: {
-			xxs: ['0.625rem', { lineHeight: '1rem' }], // 10px con un line-height de 16px
-		  },
-		screens: {
-		  'sm': {'max': '639px'}, // Esto hace que sm sea para dispositivos con un ancho máximo de 639px
-		  'md': '640px', // Puedes ajustar estos valores según sea necesario
-		  'lg': '1024px',
-		  'xl': '1280px',
-		  '2xl': '1536px',
-		},
-		typography: {
-			DEFAULT: {
-			  css: {
-				hyphens: 'auto',
-			  },
+		extend: {
+			zIndex: {
+				'0': 0,
+				'10': 10,
+				'50': 50,
+				'1000': 1000,
+				'9999': 9999,
+			},
+			spacing: {
+				'100': '25rem',
+			},
+			fontFamily: {
+				Poppins: ["Poppins", "sans-serif"],
+			},
+			fontSize: {
+				xxs: ['0.625rem', { lineHeight: '1rem' }], // 10px con un line-height de 16px
+			},
+			screens: {
+				'sm': { 'max': '639px' },
+				'md': '640px',
+				'lg': '1024px',
+				'xl': '1280px',
+				'2xl': '1536px',
+			},
+			typography: {
+				DEFAULT: {
+					css: {
+						hyphens: 'auto',
+					},
+				},
+			},
+			keyframes: {
+				fadeIn: {
+					'0%': { opacity: '0', transform: 'scale(0.9)' },
+					'100%': { opacity: '1', transform: 'scale(1)' },
+				},
+				slideIn: {
+					'0%': { opacity: '0', transform: 'translateX(100px)' }, // Desliza desde la derecha
+					'100%': { opacity: '1', transform: 'translateX(0)' },
+				},
+			},
+			animation: {
+				fadeIn: 'fadeIn 1s ease-out forwards',
+				slideIn: 'slideIn 0.8s ease-out forwards',
 			},
 		},
-	  },
 	},
 	plugins: [
-	  function ({ addUtilities }) {
-		addUtilities(
-		  {
-			'.scrollbar-hide::-webkit-scrollbar': {
-			  display: 'none',
-			},
-			'.scrollbar-hide': {
-			  '-ms-overflow-style': 'none', /* Para Internet Explorer 10+ */
-			  'scrollbar-width': 'none', /* Para Firefox */
-			},
-		  },
-		  ['responsive', 'hover']
-		)
-	  },
-	  function ({ addBase }) {
-		addBase({
-		  '*': {
-			position: 'static',
-			zIndex: 'auto',
-		  },
-		});
-	  },
+		function ({ addUtilities }) {
+			addUtilities(
+				{
+					'.scrollbar-hide::-webkit-scrollbar': {
+						display: 'none',
+					},
+					'.scrollbar-hide': {
+						'-ms-overflow-style': 'none', /* Para Internet Explorer 10+ */
+						'scrollbar-width': 'none', /* Para Firefox */
+					},
+				},
+				['responsive', 'hover']
+			)
+		},
+		function ({ addBase }) {
+			addBase({
+				'*': {
+					position: 'static',
+					zIndex: 'auto',
+				},
+			});
+		},
 	],
-  };
-  
+};
